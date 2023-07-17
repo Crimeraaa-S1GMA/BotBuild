6e97edc86a1d2ee264cac6a9c5aa9d8554495392
crimeraaa <118309721+Crimeraaa-S1GMA@users.noreply.github.com>
2023-07-17 20:44:45 +0200

    poll stuff

--- polls.py
+++ polls.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import discord
 import discord.ui
 from discord.ext import commands
@@ -5,24 +6,31 @@ from discord import app_commands
 
 import config_access
 
+class PollView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.select(custom_id="select_menu")
+    async def select(self, interaction : discord.Interaction, select : discord.ui.Select):
+        await interaction.response.send_message(f"Selected option {select.values[0]}!", ephemeral=True)
+
 class Polls(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        bot.add_view(PollView())
         print("Initialized poll cog...")
     
     @app_commands.command()
     async def poll(self, interaction : discord.Interaction, channel : discord.TextChannel, question : str, answer_1 : str, answer_2 : str, answer_3 : str = "", answer_4 : str = "", answer_5 : str = ""):
-        view = discord.ui.View()
-        select_menu = discord.ui.Select()
-        select_menu.options.append(discord.SelectOption(label=answer_1, value="1"))
-        select_menu.options.append(discord.SelectOption(label=answer_2, value="2"))
+        view = PollView()
+        view.children[0].options.append(discord.SelectOption(label=answer_1, value="1"))
+        view.children[0].options.append(discord.SelectOption(label=answer_2, value="2"))
         if len(answer_3) > 0:
-            select_menu.options.append(discord.SelectOption(label=answer_3, value="3"))
+            view.children[0].options.append(discord.SelectOption(label=answer_3, value="3"))
         if len(answer_4) > 0:
-            select_menu.options.append(discord.SelectOption(label=answer_4, value="4"))
+            view.children[0].options.append(discord.SelectOption(label=answer_4, value="4"))
         if len(answer_5) > 0:
-            select_menu.options.append(discord.SelectOption(label=answer_5, value="5"))
-        view.add_item(select_menu)
+            view.children[0].options.append(discord.SelectOption(label=answer_5, value="5"))
 
         message = await channel.send(question, view=view)
         await interaction.response.send_message(f"Poll sent!", ephemeral=True)
