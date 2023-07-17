c31e63246e5168660b3c93318b466cc508b51f83
crimeraaa <118309721+Crimeraaa-S1GMA@users.noreply.github.com>
2023-07-17 16:04:14 +0200

    Update polls.py

--- polls.py
+++ polls.py
@@ -12,6 +12,18 @@ class Polls(commands.Cog):
         print("Initialized poll cog...")
     
     @app_commands.command()
-    async def poll(self, interaction : discord.Interaction, channel : discord.TextChannel, question : str, answer_1 : str, answer_2 : str = "", answer_3 : str = "", answer_4 : str = "", answer_5 : str = ""):
-        message = await channel.send(question)
+    async def poll(self, interaction : discord.Interaction, channel : discord.TextChannel, question : str, answer_1 : str, answer_2 : str, answer_3 : str = "", answer_4 : str = "", answer_5 : str = ""):
+        view = discord.ui.View()
+        select_menu = discord.ui.Select()
+        select_menu.options.append(discord.SelectOption(label=answer_1, value="1"))
+        select_menu.options.append(discord.SelectOption(label=answer_2, value="2"))
+        if len(answer_3) > 0:
+            select_menu.options.append(discord.SelectOption(label=answer_3, value="3"))
+        if len(answer_4) > 0:
+            select_menu.options.append(discord.SelectOption(label=answer_4, value="4"))
+        if len(answer_5) > 0:
+            select_menu.options.append(discord.SelectOption(label=answer_5, value="5"))
+        view.add_item(select_menu)
+
+        message = await channel.send(question, view=view)
         await interaction.response.send_message(f"Poll sent! {message.id}", ephemeral=True)
