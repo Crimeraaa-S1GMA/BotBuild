2faf5a4c24177697b67daaa1c9bcdd1ad452e0c3
crimeraaa <118309721+Crimeraaa-S1GMA@users.noreply.github.com>
2023-07-18 13:44:22 +0200

    Update polls.py

--- polls.py
+++ polls.py
@@ -75,40 +75,54 @@ def get_poll_options(message_id : int):
 
     return [len_1, len_2, len_3, len_4, len_5, len_1 + len_2 + len_3 + len_4 + len_5, content, answer_1, answer_2, answer_3, answer_4, answer_5]
 
+def has_poll(message_id : int):
+    with open("bot_data.json", "r") as file_r:
+        bot_data = file_r.read()
+    
+    bot_data_json = json.loads(bot_data)
+
+    if "polls" in bot_data_json:
+        return str(message_id) in bot_data_json["polls"]
+    else:
+        return False
+
 class PollView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
     @discord.ui.select(custom_id="select_menu")
     async def select(self, interaction : discord.Interaction, select : discord.ui.Select):
-        choice_to_json_choice = "choice1Users"
-        if select.values[0] == "1":
+        if has_poll(interaction.message.id):
             choice_to_json_choice = "choice1Users"
-        if select.values[0] == "2":
-            choice_to_json_choice = "choice2Users"
-        if select.values[0] == "3":
-            choice_to_json_choice = "choice3Users"
-        if select.values[0] == "4":
-            choice_to_json_choice = "choice4Users"
-        if select.values[0] == "5":
-            choice_to_json_choice = "choice5Users"
-        vote_on_poll(interaction.message.id, interaction.user.id, choice_to_json_choice)
-        poll_options = get_poll_options(interaction.message.id)
-        message_content = f"# **Poll: {poll_options[6]}**\n"
-        message_content += f"\n{poll_options[7]} - `{(poll_options[0] / poll_options[5]) * 100.0}%`"
-        message_content += f"\n{poll_options[8]} - `{(poll_options[1] / poll_options[5]) * 100.0}%`"
-        if len(poll_options[9]) > 0:
-            message_content += f"\n{poll_options[9]} - `{(poll_options[2] / poll_options[5]) * 100.0}%`"
-        if len(poll_options[10]) > 0:
-            message_content += f"\n{poll_options[10]} - `{(poll_options[3] / poll_options[5]) * 100.0}%`"
-        if len(poll_options[11]) > 0:
-            message_content += f"\n{poll_options[11]} - `{(poll_options[4] / poll_options[5]) * 100.0}%`"
-        if poll_options[5] == 1:
-            message_content += f"\n\n`1 person voted on this poll`"
+            if select.values[0] == "1":
+                choice_to_json_choice = "choice1Users"
+            if select.values[0] == "2":
+                choice_to_json_choice = "choice2Users"
+            if select.values[0] == "3":
+                choice_to_json_choice = "choice3Users"
+            if select.values[0] == "4":
+                choice_to_json_choice = "choice4Users"
+            if select.values[0] == "5":
+                choice_to_json_choice = "choice5Users"
+            vote_on_poll(interaction.message.id, interaction.user.id, choice_to_json_choice)
+            poll_options = get_poll_options(interaction.message.id)
+            message_content = f"# **Poll: {poll_options[6]}**\n"
+            message_content += f"\n{poll_options[7]} - `{(poll_options[0] / poll_options[5]) * 100.0}%`"
+            message_content += f"\n{poll_options[8]} - `{(poll_options[1] / poll_options[5]) * 100.0}%`"
+            if len(poll_options[9]) > 0:
+                message_content += f"\n{poll_options[9]} - `{(poll_options[2] / poll_options[5]) * 100.0}%`"
+            if len(poll_options[10]) > 0:
+                message_content += f"\n{poll_options[10]} - `{(poll_options[3] / poll_options[5]) * 100.0}%`"
+            if len(poll_options[11]) > 0:
+                message_content += f"\n{poll_options[11]} - `{(poll_options[4] / poll_options[5]) * 100.0}%`"
+            if poll_options[5] == 1:
+                message_content += f"\n\n`1 person voted on this poll`"
+            else:
+                message_content += f"\n\n`{poll_options[5]} people voted on this poll`"
+            await interaction.message.edit(content=message_content)
+            await interaction.response.send_message(f"Selected option {select.values[0]}!", ephemeral=True)
         else:
-            message_content += f"\n\n`{poll_options[5]} people voted on this poll`"
-        await interaction.message.edit(content=message_content)
-        await interaction.response.send_message(f"Selected option {select.values[0]}!", ephemeral=True)
+            await interaction.response.send_message("Could not find poll data in bot cache", ephemeral=True)
 
 class Polls(commands.Cog):
     def __init__(self, bot):
