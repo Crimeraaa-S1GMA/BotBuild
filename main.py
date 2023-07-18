5ba888625d8d34d540a2bef48a0cbc6c8724d93d
crimeraaa <118309721+Crimeraaa-S1GMA@users.noreply.github.com>
2023-07-18 15:30:27 +0200

    dashboard test

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route("/")
 def main():
-    return "<h1>Working</h1>"
+    return render_template("index.html")
 
 app.run()
