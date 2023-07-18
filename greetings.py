45ab83dcce26011474764937652311bfc1d7a2e5
crimeraaa <118309721+Crimeraaa-S1GMA@users.noreply.github.com>
2023-07-18 14:39:01 +0200

    fix greetings

--- greetings.py
+++ greetings.py
@@ -15,7 +15,7 @@ class Greetings(commands.Cog):
         if config_access.return_config_value("welcome_module_enabled") and config_access.return_config_value("welcome_module_user_join_enabled") \
                 and member.guild == self.bot.get_channel(config_access.return_config_value("welcome_module_join_channel")).guild:
             channel = self.bot.get_channel(config_access.return_config_value("welcome_module_join_channel"))
-            await channel.send(config_access.return_config_value("welcome_module_join_message").format(member_mention=member.mention))
+            await channel.send(config_access.return_config_value("welcome_module_user_join_message").format(member_mention=member.mention))
             guild = member.guild
             for role_id in config_access.return_config_value("welcome_module_join_role_ids"):
                 role = guild.get_role(role_id)
@@ -26,4 +26,4 @@ class Greetings(commands.Cog):
         if config_access.return_config_value("welcome_module_enabled") and config_access.return_config_value("welcome_module_user_leave_enabled") \
                 and member.guild == self.bot.get_channel(config_access.return_config_value("welcome_module_leave_channel")).guild:
             channel = self.bot.get_channel(config_access.return_config_value("welcome_module_leave_channel"))
-            await channel.send(config_access.return_config_value("welcome_module_leave_message").format(member_mention=member.mention))
+            await channel.send(config_access.return_config_value("welcome_module_user_leave_message").format(member_mention=member.mention))
