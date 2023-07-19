97bcdf5e8b2f5604bbc944a3a2f225f103d14972
crimeraaa <118309721+Crimeraaa-S1GMA@users.noreply.github.com>
2023-07-19 14:29:09 +0200

    bot restart on token change

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ import greetings
 class BotBuildClient(commands.AutoShardedBot):
     async def on_ready(self):
         activity = discord.Activity(name="Made with BotBuild", type=discord.ActivityType.watching)
-        await bot.change_presence(status=discord.Status.online, activity=activity)
+        await self.change_presence(status=discord.Status.online, activity=activity)
         print("Welcome to BotBuild!")
         print("__________________________\n")
         print(f'Account: {self.user.name}!')
@@ -25,26 +25,24 @@ class BotBuildClient(commands.AutoShardedBot):
         print("Initializing cogs...")
         
         if config_access.return_config_value("moderation_module_enabled"):
-            await bot.add_cog(moderation.Moderation(self))
+            await self.add_cog(moderation.Moderation(self))
         
         if config_access.return_config_value("meme_channel_module_enabled"):
-            await bot.add_cog(memechannel.MemeChannel(self))
+            await self.add_cog(memechannel.MemeChannel(self))
         
         if config_access.return_config_value("sus_mode_easter_egg"):
-            await bot.add_cog(susmode.SusMode(self))
+            await self.add_cog(susmode.SusMode(self))
         
         if config_access.return_config_value("polls_module_enabled"):
-            await bot.add_cog(polls.Polls(self))
+            await self.add_cog(polls.Polls(self))
         
         if config_access.return_config_value("welcome_module_enabled"):
-            await bot.add_cog(greetings.Greetings(self))
+            await self.add_cog(greetings.Greetings(self))
         
-        await bot.add_cog(debug.Debug(self))
+        await self.add_cog(debug.Debug(self))
 
         print("__________________________\n")
         print("Ready!")
-
-
 
 token_file = open("token.txt", "r")
 loaded_token = token_file.read()
@@ -53,6 +51,10 @@ token_file.close()
 
 intents = discord.Intents.all()
 
-bot = BotBuildClient(intents=intents, command_prefix="!")
+bot_client = BotBuildClient(intents=intents, command_prefix="!")
+try:
+    bot_client.run(loaded_token)
+except:
+    print("Bot login failed. Check if you entered the correct token in the dashboard or the token.txt file.")
 
-bot.run(loaded_token)
+
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request
 import os
 import json
+import subprocess
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
+
+subprocess_instance = None
 
 @app.route("/")
 def main():
@@ -13,6 +16,11 @@ def bot_credentials_set():
     payload = request.get_json()
     with open("token.txt", "w+") as file:
         file.write(payload["token"])
+    global subprocess_instance
+    if subprocess_instance is not None:
+        subprocess_instance.terminate()
+        subprocess_instance.wait()
+        subprocess_instance = subprocess.Popen(["python3", "bot.py"])
     return payload["token"]
 
 @app.route("/settings", methods=["GET"])
@@ -28,6 +36,7 @@ def bot_credentials_get():
 
     return data_json
 
+subprocess_instance = subprocess.Popen(["python3", "bot.py"])
 
 if __name__ == "__main__":
     app.run()
