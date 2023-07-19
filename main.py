a217f49458d60acd3d135245229c57aa77ed2f92
crimeraaa <118309721+Crimeraaa-S1GMA@users.noreply.github.com>
2023-07-19 13:32:17 +0200

    dashboard basis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
+import json
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
 
@@ -6,4 +8,26 @@ app = Flask(__name__, template_folder="templates", static_folder="static")
 def main():
     return render_template("index.html")
 
-app.run()
+@app.route("/bot-credentials/set", methods=["POST"])
+def bot_credentials_set():
+    payload = request.get_json()
+    with open("token.txt", "w+") as file:
+        file.write(payload["token"])
+    return payload["token"]
+
+@app.route("/settings", methods=["GET"])
+def bot_credentials_get():
+    token = ""
+    if os.path.exists("token.txt"):
+        with open("token.txt", "r") as file:
+            token = file.read()
+    data = {
+        "token" : token
+    }
+    data_json = json.dumps(data)
+
+    return data_json
+
+
+if __name__ == "__main__":
+    app.run()
--- static/index.js
+++ static/index.js
@@ -3,3 +3,43 @@ var myDiv = document.getElementById('sidebar-button-1');
 myDiv.addEventListener('click', function() {
   alert('test');
 });
+
+function loadInitialValues() {
+  var url = '/settings';
+  fetch(url, {
+    method: 'GET'
+  })
+    .then(response => response.json())
+    .then(responseData => {
+      console.log(responseData);
+      document.getElementById("bot-credentials-token").setAttribute("value", responseData.token)
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+
+function botCredentialsSave() {
+  let tokenValue = document.getElementById("bot-credentials-token").value;
+
+  let data = {
+    token : tokenValue
+  };
+  let serializedData = JSON.stringify(data);
+
+  var url = '/bot-credentials/set';
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: serializedData
+  })
+    .then(response => response.text())
+    .then(responseData => {
+      console.log(responseData);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
